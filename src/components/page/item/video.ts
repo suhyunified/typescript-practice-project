db42720d3ed651eef8ea915ce5cda99e0fb551a7
Suhyun Lim <shelly@deepnatural.ai>
2021-02-13 18:32:09 +0900

    feat: add video component
    - Video has title and a Youtube url address.

---
+++ src/components/page/item/video.ts
@@ -0,0 +1,32 @@
+import { BaseComponent } from "../../component/base.js";
+
+type Video = {
+  title: string;
+  url: string;
+};
+
+export class VideoComponent extends BaseComponent<HTMLElement> {
+  constructor(video: Video) {
+    const innerHTML = `<section class="item video">
+        <p class="video__title"></p>
+        <div class="video__holder">
+            <iframe class="video__thumbnail"></iframe>
+
+        </div>
+    </section>`;
+
+    super(innerHTML);
+
+    const titleElement = this.element.querySelector(
+      ".video__title"
+    )! as HTMLParagraphElement;
+
+    titleElement.textContent = video.title;
+
+    const iframeElement = this.element.querySelector(
+      ".video__thumbnail"
+    )! as HTMLEmbedElement;
+
+    iframeElement.src = video.url;
+  }
+}
